0e321a10f961d41e740a02cc5839c9e95d41128e
Pr0pyh <andrej.drmanovic@gmail.com>
2024-01-27 04:29:30 +0100

    ubaceni leveli

---
+++ scenes/Kutija/Kutija.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class Kutija : Area3D
+{
+    AnimationPlayer animPlayer;
+    public override void _Ready()
+    {
+        animPlayer = GetNode<Node3D>("toolbox").GetNode<AnimationPlayer>("AnimationPlayer2");
+    }
+    public void open(Player player)
+    {
+        GD.Print("otvaranje");
+        player.levelUp();
+        animPlayer.Play("kutijaAnimations/opening");
+    }
+
+    public void _on_body_entered(Node3D body)
+    {
+        if(body.GetType() == typeof(Player))
+        {
+            GD.Print(body);
+            ((Player)body).canOpen = true;
+            ((Player)body).usableObject = this;
+        }
+    }
+
+    public void _on_body_exited(Node3D body)
+    {
+        if(body.GetType() == typeof(Player))
+        {
+            ((Player)body).canOpen = false;
+            ((Player)body).usableObject = null;
+        }
+    }
+}
--- scenes/Player/Player.cs
+++ scenes/Player/Player.cs
@@ -16,17 +16,25 @@ public partial class Player : CharacterBody3D
 	Godot.Vector3 moveVector;
 	Camera3D camera;
 	public RayCast3D raycast;
-    Gun gun;
-	Pecaljka pecaljka;
-	Item item;
+    public Gun gun;
+	public Pecaljka pecaljka;
+	public Item item;
+	public bool canOpen;
+	public int levelCounter;
+	public Node3D usableObject;
+	Item[] items;
 	public override void _Ready()
 	{
 		state = STATE.WALK;
 		camera = GetNode<Camera3D>("Camera3D");
         pecaljka = camera.GetNode<Pecaljka>("Pecaljka");
+		gun = camera.GetNode<Gun>("Gun");
 		raycast = camera.GetNode<RayCast3D>("RayCast3D");
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 		item = pecaljka;
+		canOpen = false;
+		levelCounter = 0;
+		items = new Item[2]{pecaljka, gun};
 	}
 
     public override void _Input(InputEvent @event)
@@ -46,6 +54,7 @@ public partial class Player : CharacterBody3D
 			case STATE.WALK:
 				walkState(delta);
                 shootState(delta);
+				useState();
 				break;
 			case STATE.FISH:
 				fishState(delta);
@@ -90,10 +99,24 @@ public partial class Player : CharacterBody3D
             item.shoot(this);
     }
 
+	public void useState()
+	{
+		if(canOpen && Input.IsActionJustPressed("use") && usableObject != null)
+			((Kutija)usableObject).open(this);
+	}
+
 	public void quitState()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Visible;
 		if(Input.IsActionJustPressed("exit"))
 			GetTree().Quit();
+	}
+
+	public void levelUp()
+	{
+		levelCounter++;
+		item.Visible = false;
+		item = items[levelCounter];
+		item.Visible = true;
 	}
 }
