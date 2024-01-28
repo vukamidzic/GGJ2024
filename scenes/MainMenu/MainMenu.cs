7b506f659842afad8ec64d7f667c7f49752fc29b
VukAmidzic <vuk.amidzic@gmail.com>
2024-01-28 05:19:50 +0100

    remove terrain files

--- scenes/MainMenu/MainMenu.cs
+++ scenes/MainMenu/MainMenu.cs
@@ -3,17 +3,17 @@ using System;
 
 public partial class MainMenu : Control
 {
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Visible;
-    }
-    public void _on_button_pressed()
-    {
-        GetTree().ChangeSceneToFile("res://scenes/Level.tscn");
-    }
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Visible;
+	}
+	public void _on_button_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/Level.tscn");
+	}
 
-    public void _on_button_2_pressed()
-    {
-        GetTree().Quit();
-    }
+	public void _on_button_2_pressed()
+	{
+		GetTree().Quit();
+	}
 }
